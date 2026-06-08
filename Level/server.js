6d03190503a6cee744158af0b769c096da2a5290
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-08 07:36:08 +0530

    small update in server

--- Level/server.js
+++ Level/server.js
@@ -1,14 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const port = process.env.PORT || 3000
+
 const app = express()
-const port = 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.post('/', (req, res) => {
     console.log(req.body)
     res.json({ success: true })
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
+app.listen(port, () => { 
+    console.log(`Example app listening at http://localhost:3000`)
+})
