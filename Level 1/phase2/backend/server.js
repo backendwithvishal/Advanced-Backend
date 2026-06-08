87c3105e940bd8fd7354ef791ded1fcfdb6f09d0
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-09 00:19:00 +0530

    small update phase 2 file & folder

--- Level 1/phase2/backend/server.js
+++ Level 1/phase2/backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const app = express()
 
